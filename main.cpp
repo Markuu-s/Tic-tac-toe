cd6edec804e132e585b0ee0c39bc39e4f3411871
Markuu-s <qwert11223381@gmail.com>
2022-01-03 19:26:49 +0300

    [main] Add main for test game.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+#include "ConsoleDrawer.hpp"
+#include "GameLogic.hpp"
+#include <string>
+#include <iostream>
+
 int main()
 {
-    
+    GameLogic game;
 
+    Drawer *drawer = new ConsoleDrawer();
+    bool gameBool = true;
+    while (gameBool)
+    {
+        drawer->draw(game.getFieldGame());
+        std::string userMove;
+        std::cin >> userMove;
+        std::string move = "MOVE_" + userMove;
+        int a = game.makeMove(move);
+        std::cout << a;
+        gameBool = !a;
+    }
     return 0;
 }
